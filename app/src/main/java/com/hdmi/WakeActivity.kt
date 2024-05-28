678e62662b2463c475762e90ed8bb846546dee46
Jimmy <2947244682@qq.com>
2024-05-28 21:59:12 +0800

    可以用MD了

--- app/src/main/java/com/hdmi/WakeActivity.kt
+++ app/src/main/java/com/hdmi/WakeActivity.kt
@@ -8,6 +8,8 @@ import android.os.Bundle
 class WakeActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+//        setContentView(R.layout.activity_wake)
         if (GlobalUtils.packageName != "" && GlobalUtils.displayId >= 0) {
             AppUtils.launchApp(this, GlobalUtils.packageName, GlobalUtils.displayId)
             moveTaskToBack(true)
