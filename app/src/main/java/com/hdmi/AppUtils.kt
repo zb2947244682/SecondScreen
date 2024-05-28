e327977a5bf1872b4b20a3c20d397d0183758745
Jimmy <2947244682@qq.com>
2024-05-28 18:49:38 +0800

    123

--- app/src/main/java/com/hdmi/AppUtils.kt
+++ app/src/main/java/com/hdmi/AppUtils.kt
@@ -4,22 +4,20 @@ import android.content.Context
 import android.content.pm.ApplicationInfo
 import android.content.pm.PackageManager
 
-class AppUtils {
-    companion object {
-        fun getApps(context: Context): List<ApplicationInfo> {
-            val packageManager = context.packageManager
-            val apps = packageManager.getInstalledApplications(PackageManager.GET_META_DATA)
-            val userInstalledApps = mutableListOf<ApplicationInfo>()
+object AppUtils {
+    fun getApps(context: Context): List<ApplicationInfo> {
+        val packageManager = context.packageManager
+        val apps = packageManager.getInstalledApplications(PackageManager.GET_META_DATA)
+        val userInstalledApps = mutableListOf<ApplicationInfo>()
 
-            for (app in apps) {
-                if ((app.flags and ApplicationInfo.FLAG_SYSTEM) == 0) {
-                    // Exclude system apps
-                    app.name = app.loadLabel(packageManager).toString()
-                    userInstalledApps.add(app)
-                }
+        for (app in apps) {
+            if ((app.flags and ApplicationInfo.FLAG_SYSTEM) == 0) {
+                // Exclude system apps
+                app.name = app.loadLabel(packageManager).toString()
+                userInstalledApps.add(app)
             }
-
-            return userInstalledApps
         }
+
+        return userInstalledApps
     }
 }
